6f8487116de27a5d0a10f059b71075f8f8b80b7f
Samuel Macharia <67005722+Engeniusam@users.noreply.github.com>
2025-10-25 17:32:09 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
   MongoURI:
     // "mongodb+srv://admin:root123@cluster0.wpq4kw9.mongodb.net/?retryWrites=true&w=majority",
-    "mongodb+srv://Engeniusam:Fsociety.net1@cluster0.oeubewp.mongodb.net/",
+    "mongodb+srv://Engeniusam:Fsociety.net1@cluster0.oeubewp.mongodb.net/luxury-salon?retryWrites=true&w=majority
+",
 };
